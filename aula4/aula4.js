936753f42782633e8ba176a23ceec18136a1cdff
João Gabryel Cardoso <joaogabryel1245@gmail.com>
2025-12-18 09:51:16 -0300

    Upload aula 4

---
+++ aula4/aula4.js
@@ -0,0 +1,52 @@
+// function escreveNome(){
+//     console.log("Gabryel")
+// }
+
+// escreveNome()
+
+/*Função que recebe parâmetros*/
+// function escreveNome(nome){
+//     console.log(nome)
+// }
+
+// escreveNome("Gbb")
+
+// function soma(numero1, numero2){
+//     const resultado = numero1 + numero2
+//     console.log(resultado)
+// }
+
+// soma(4,5)
+// soma(2,4)
+// soma(9,6)
+/*
+function soma(numero1, numero2){
+    const resultado = numero1 + numero2
+    return resultado
+}
+
+const resultadaoDaSoma = soma(10,20)
+console.log(resultadaoDaSoma)
+*/
+
+/*
+function estaEndividado(receita, gastos){
+    if(receita > gastos){
+        return "Está tudo certo! Não endividado(a)"
+    } else {
+        return"CUIDADO! Está ou está no caminho para se endividar"
+    }
+}
+
+const maria = estaEndividado(1500,600)
+const joao = estaEndividado(1200,1500)
+
+console.log(maria)
+console.log(joao)
+*/
+
+const Exemplo = () => {
+	console.log("exemplo2")
+}
+
+Exemplo()
