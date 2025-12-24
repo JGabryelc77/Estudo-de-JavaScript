ef2afa9176f108f0fd2657a701c1079295a05ab4
João Gabryel Cardoso <joaogabryel1245@gmail.com>
2025-12-24 09:55:14 -0300

    estudos math

---
+++ aula7/math.js
@@ -0,0 +1,10 @@
+/*
+const calculo = Math.pow(2,5)
+console.log(calculo)
+
+const calculo = Math.sqrt(125,2)
+console.log(Math.floor(calculo))
+*/
+
+const calculo = Math.random()
+console.log(calculo)
