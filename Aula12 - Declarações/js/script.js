3ec0de4dc4b742de614df2127bc0c9b5cde1a207
João Gabryel Cardoso <joaogabryel1245@gmail.com>
2025-12-30 09:23:51 -0300

    Declarações

---
+++ Aula12 - Declarações/js/script.js
@@ -0,0 +1,18 @@
+let a,b,c;
+a = 6;
+b = 15;
+c = 9;
+
+/* OU PODE SER DECLARADO ASSIM:
+let a,b,c; a = 6; b = 15; c = 9;
+*/
+
+/*Java script desconsidera os espaços:
+let pessoa = "teste"
+let pessoa="teste  - São a mesma coisa para o JS
+*/
+
+/* Mesmo quebrando linhas o js vai executar contando que tenha a terminação ";" */
+
+function minhaFuncao(){
+    document.write(5+5);
