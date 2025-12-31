80e1fbdbc66cd97e31d978b542fec92089d23744
João Gabryel Cardoso <joaogabryel1245@gmail.com>
2025-12-31 13:18:18 -0300

    Sintaxe js

---
+++ Aula13 - Sintaxe/js/script.js
@@ -0,0 +1,26 @@
+/*podemos ter vários tipos diferentes de conteúdo: Texto, números e etc*/
+
+console.log(5+5); // Número
+console.log("5+5"); // String
+console.log("1"); // String
+console.log(1); // Número
+
+/* " " Aspas duplas textos, ' ' textos também */
+
+var a = 5;
+var b = 9;
+var c = a + b;
+console.log(c);
+
+/*JS Case sensitivy, sensível a variáveis com letra maiúscula e minúscula */
+var teste = "Minúsculo";
+var Teste = "Maiúsculo";
+
+console.log(teste);
+
+/* Na Sintaxe do js não podemos usar hífen 
+Ex :*/
+var primeiro-nome; //Ele entenderia que quer subtrair um pelo outro
+//Correto:
+var primeiroNome;
+
