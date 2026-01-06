7b44c937a6c80b26adeedd74a1d3bc316f95cdcf
João Gabryel Cardoso <joaogabryel1245@gmail.com>
2026-01-06 17:42:15 -0300

    Aula comentários

---
+++ Aula14 - Comentários/js/script.js
@@ -0,0 +1,7 @@
+/* Tipos :
+Duas barras inclinadas => // (Não aceita quebra de linha)
+*/
+
+/*Barra e asterisco e asterisco barra para fechar (Aceita quebra de linhas)*/
+
+//Inserindo um texto no código.
